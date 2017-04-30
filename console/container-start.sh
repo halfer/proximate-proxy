e6a437f01c55fd5e077ce16bd232a1fb63a2abbf
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-30 21:16:31 +0100

    Whoops, get the command right for Alpine

--- console/container-start.sh
+++ console/container-start.sh
@@ -2,4 +2,4 @@
 
 # Write to both stdout and a log file
 echo "Teeing proxy logs also to $PROXY_LOG_PATH"
-php console/proxy-server.php | tee --append $PROXY_LOG_PATH
+php /var/proxy/console/proxy-server.php | tee -a $PROXY_LOG_PATH
